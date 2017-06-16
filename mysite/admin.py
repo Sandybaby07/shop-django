e43ca71a0e4b07f36197d7ea7079b8b1db68a7d2
Sandybaby07 <weichihsung@icloud.com>
2017-06-16 22:14:53 +0800

    Update admin.py

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from mysite import models
+from mysite.models import Product, Category, Order, OrderItem
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('category', 'sku', 'name', 'stock', 'price')
     ordering = ('category',)
 
-admin.site.register(models.Product, ProductAdmin)
-admin.site.register(models.Category)
-admin.site.register(models.Order)
-admin.site.register(models.OrderItem)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(OrderItem)
